21e482cc4869ed63b51f2b8f4ef5bd0113600319
ramonsales <ramon.silvasales@gmail.com>
2018-06-15 16:36:15 -0300

    Add sidebars to the footer

--- wp-content/themes/temateste/footer.php
+++ wp-content/themes/temateste/footer.php
@@ -13,7 +13,12 @@
             <?php endif; ?>
             <?php if ( is_active_sidebar( 'footer_sidebar_3' ) ) : ?>
                 <div id="primary-sidebar" class="col-md-4 primary-sidebar widget-area" role="complementary">
-                    <?php dynamic_sidebar( 'footer_sidebar_3' ); ?>
+
+                    <?php 
+                        dynamic_sidebar( 'footer_sidebar_3' );
+                        // echo do_shortcode('[contact-form-7 id="65"]');
+
+                    ?>
                 </div>
             <?php endif; ?>
             </div>
